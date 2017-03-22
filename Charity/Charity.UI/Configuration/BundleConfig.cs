8babd22a5f6917c40df57ce82e0ded9a7112cbae
AiyeroKing <AiyeroKing@outlook.com>
2017-03-22 15:28:37 +0800

    Charity.UI—导入bootstrap和Iview样式和JS

--- Charity/Charity.UI/Configuration/BundleConfig.cs
+++ Charity/Charity.UI/Configuration/BundleConfig.cs
@@ -13,9 +13,6 @@ namespace Charity.UI
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -25,11 +22,8 @@ namespace Charity.UI
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/css/bootstrap.css",
-                      "~/Content/css/iview.css",
-                      "~/Content/css/Layout.css"));
-
-            
+                      "~/Content/css/bootstrap/bootstrap.css",
+                      "~/Content/css/iview/iview.css"));
         }
     }
 }
