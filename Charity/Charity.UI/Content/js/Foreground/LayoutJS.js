8ad87fd09cba8ebd74f626ed9717168a6fb1193f
AiyeroKing <AiyeroKing@outlook.com>
2017-03-23 16:33:58 +0800

    Charity.UI—最顶层导航栏弹出二维码图片

--- Charity/Charity.UI/Content/js/Foreground/LayoutJS.js
+++ Charity/Charity.UI/Content/js/Foreground/LayoutJS.js
@@ -1,3 +1,10 @@
-﻿new Vue({
-    el: '#Topside',
-})
+﻿//当鼠标移动到此位置事件，显示dispaly:black;
+function Charityover(id) {
+    var styledisplay = document.getElementById(id);
+    styledisplay.style.display = 'block';
+}
+//当鼠标移动到此位置事件，显示dispaly:none;
+function Charityoff(id) {
+    var styledisplay = document.getElementById(id);
+    styledisplay.style.display = 'none';
+}
