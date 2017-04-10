9e64f6b44de739d9507b85cc1ddbf7329c18a369
AiyeroKing <AiyeroKing@outlook.com>
2017-04-10 15:02:32 +0800

    Charity——前台登录、后台登录页面已完成

---
+++ Charity/Charity.UI/Controllers/Foreground/LoginSignUpController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Charity.UI.Controllers.Foreground
+{
+    public class LoginSignUpController : Controller
+    {
+        #region --登录注册页面显示返回值
+        // GET: LoginSignUp
+        //public ActionResult LoginSignUpIndex()
+        //{
+        //    return RedirectToAction("LoginIndex", "LoginSignUp");
+        //    //return View();
+        //}
+        /// <summary>
+        /// 登录页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoginIndex()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 注册页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SignUpIndex()
+        {
+            return View();
+        }
+
+        public ActionResult UserLoginIndex()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
