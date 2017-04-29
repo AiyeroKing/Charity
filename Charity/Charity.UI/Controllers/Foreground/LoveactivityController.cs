9e68e480dea1fbe183ece3ee7748f80b4ddeee3e
AiyeroKing <AiyeroKing@outlook.com>
2017-04-29 11:57:44 +0800

    Charity - 爱心活动已完成

--- Charity/Charity.UI/Controllers/Foreground/LoveactivityController.cs
+++ Charity/Charity.UI/Controllers/Foreground/LoveactivityController.cs
@@ -8,8 +8,28 @@ namespace Charity.UI.Controllers.Foreground
 {
     public class LoveactivityController : Controller
     {
+        /// <summary>
+        /// 爱心活动首页--之爱心教育
+        /// </summary>
+        /// <returns></returns>
         // GET: Loveactivity
         public ActionResult LoveactivityIndex()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 爱心活动之爱心帮扶
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LovehelpIndex()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 爱心活动之特殊帮助
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LovemeatIndex()
         {
             return View();
         }
