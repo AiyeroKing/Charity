8523e85dad1cf75dce3f60d68175f44245db6343
Visual <Visual@VISUAL-PC>
2017-05-17 11:12:55 +0800

    Charity - 前台留言板数据接通

--- Charity/Charity.UI/Controllers/Foreground/CapitalController.cs
+++ Charity/Charity.UI/Controllers/Foreground/CapitalController.cs
@@ -8,10 +8,17 @@ namespace Charity.UI.Controllers.Foreground
 {
     public class CapitalController : Controller
     {
+        #region -- 返回页面
+
+        /// <summary>
+        /// 资金捐助
+        /// </summary>
         // GET: Capital
+
         public ActionResult CapitalIndex()
         {
             return View();
         }
+        #endregion
     }
 }
--- Charity/Charity.UI/Controllers/Foreground/ContactUsController.cs
+++ Charity/Charity.UI/Controllers/Foreground/ContactUsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Charity.Bll.Bend;
+using Charity.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,16 +9,41 @@ using System.Web.Mvc;
 namespace Charity.UI.Controllers.Foreground
 {
     public class ContactUsController : Controller
-    {
-        // GET: ContactUs
+    {  
+        // GET: Capital
+
+        #region -- 返回页面
+        /// <summary>
+        /// 联系我们
+        /// </summary>
         public ActionResult ContactUsIndex()
         {
             return View();
         }
-
+        /// <summary>
+        /// 留言板
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ContactBoardIndex()
         {
             return View();
         }
+        #endregion
+
+        #region --添加功能
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddGuestbookMSG(TGuestbook model)
+        {
+            BendGuestbookBll _bendguestbookBll = new BendGuestbookBll();
+            var result = false;
+            result = _bendguestbookBll.AddGuestbookMSG(model);
+            return RedirectToAction("HomeIndex", "Home");
+        }
+        #endregion
     }
 }
